54f41f5275479a0affa0453189da75f326c25a55
Amrendra <leostereo1108@gmail.com>
2025-01-02 21:34:15 +0530

    Update page.jsx

--- app/page.jsx
+++ app/page.jsx
@@ -4,12 +4,12 @@ export default function Home() {
   return (
     <div className="flex flex-col justify-between">
       <section className="">
-        <div className="container mx-auto text-3xl items-center justify-center flex flex-col mt-[30vh] mb-2">
+        <div className="container mx-auto text-3xl items-center justify-center flex flex-col mt-[20vh] mb-2">
           <h1 className="text-2xl font-semibold bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
-            Welcome! calculate your SGPA,
+            Welcome! predict your SGPA,
           </h1>
           <h2 className="text-2xl p-3 font-semibold bg-gradient-to-r from-blue-500 to-purple-500 bg-clip-text text-transparent">
-            with <span className="text-5xl">ease </span>&<span className="text-5xl"> accuracy</span>.
+            <span className="text-5xl">ease </span>&<span className="text-5xl"> accuracy</span>.
           </h2>
         </div>
       </section>

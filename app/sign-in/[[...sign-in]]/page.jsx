f6beb372ec932de1c1f362ea254a0f575cf2e399
Amrendra Vikram Singh <76041208+AmrendraTheCoder@users.noreply.github.com>
2025-02-18 22:57:16 +0530

    authPageTest1

--- app/sign-in/[[...sign-in]]/page.jsx
+++ app/sign-in/[[...sign-in]]/page.jsx
@@ -1,12 +1,12 @@
 import { SignIn } from '@clerk/nextjs'
 
 export default function Page() {
-    return ( 
-        <div className='flex item-center justify-center h-full pt-20 pr-20'>
+    return (
+        <div className='flex item-center justify-center h-full pt-5 p-20'>
 
             <SignIn afterSignOutUrl='/' />
 
         </div>
     )
-    
+
 }
--- app/sign-up/[[...sign-up]]/page.jsx
+++ app/sign-up/[[...sign-up]]/page.jsx
@@ -2,8 +2,8 @@ import { SignUp } from '@clerk/nextjs'
 
 export default function Page() {
     return (
-        <div className='flex item-center justify-center h-full pt-20 pr-20'>
-            <SignUp afterSignOutUrl='/'/>
+        <div className='flex item-center justify-center h-full pt-5 p-20'>
+            <SignUp afterSignOutUrl='/' />
 
         </div>
     )

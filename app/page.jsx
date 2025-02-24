da4999f82bf7b225a52fd7c89f017399a5edcc60
Amrendra Vikram Singh <76041208+AmrendraTheCoder@users.noreply.github.com>
2025-02-24 10:48:22 +0530

    ui Update

--- app/page.jsx
+++ app/page.jsx
@@ -19,7 +19,7 @@ export default function Home() {
       </section>
       <button
         onClick={() => router.push("/calculator")}
-        className="mt-6 px-6 py-3 mb-8 bg-gradient-to-r from-blue-500 to-purple-500 text-white shadow-lg transition-all duration-300 ease-in-out hover:shadow-xl hover:from-blue-600 hover:to-purple-600 hover:scale-105 rounded-md hover:bg-accent transition-colors font-medium mx-auto"
+        className="mt-6 px-6 py-3 mb-8 bg-gradient-to-r from-blue-500 to-purple-500 text-white shadow-lg transition-all duration-300 ease-in-out hover:shadow-xl hover:from-blue-600 hover:to-purple-600 hover:scale-105 rounded-md hover:bg-accent font-medium mx-auto"
       >
         Predictor
       </button>
